c280f9ec0299c934ad807f55cc714c64283e6c9e
Chenxi Zhang <xifineee@gmail.com>
2019-02-18 15:11:45 +0000

    Now using BigInteger

--- Labs/Lab2/src/ie/dit/syssecurity/lab2/RSA.java
+++ Labs/Lab2/src/ie/dit/syssecurity/lab2/RSA.java
@@ -1,27 +1,39 @@
 package ie.dit.syssecurity.lab2;
 
+import java.math.BigInteger;
 import java.util.Random;
+
+/*
+ * Program implementing the RSA Algorithm
+ * Using BigInteger and try to encrypt and
+ * decrypt using the RSA Algorithm
+ * 
+ * Date: 18/2/2019
+ * Author: Chenxi Zhang
+ */
 
 public class RSA {
 	
-	public boolean checkPrime(long num) {
+	// Implementataion of the RSA Algorithm
+	public static void generateKey() {
 		
-		for (long i = 2;  i < Math.sqrt(num); i++) {
-			// Number passed in is not prime
-			if (num % i == 0) {
-				return false;
-			}
-		}
-		
-		return true;
 	}
 	
 	public static void main(String args[]) {
+		// Generate two larget random numbers
+		BigInteger num1 = BigInteger.probablePrime(512, new Random());
+		BigInteger num2 = BigInteger.probablePrime(512, new Random());
 		
-		// Generate two large random integers
-		long num1 = new Random().nextLong();
-		long num2 = new Random().nextLong();
 		
+		// Test the number generated is prime number
+		if (num1.isProbablePrime(100) && num1.isProbablePrime(100)) {
+			System.out.println("Numbers are all prime");
+			
+			// Generating the public key and private key
+		} else {
+			System.out.println("Not all numbers are prime");
+			return;
+		}
 		
 	}
 	
